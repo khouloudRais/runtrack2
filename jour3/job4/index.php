0da79eef02639e2331999f689462567e3a50ced6
khouloudRais <khouloud.kechichhe-rais@laplateforme.io>
2025-01-15 13:59:33 +0100

    job4

---
+++ jour3/job4/index.php
@@ -0,0 +1,10 @@
+<?php
+// Création de la chaîne de caractères
+$str = "Dans l'espace, personne ne vous entend crier";
+
+// Comptage du nombre de caractères dans la chaîne
+$nombre_de_caracteres = strlen($str);
+
+// Affichage du nombre de caractères
+echo "Le nombre de caractères dans la chaîne est : " . $nombre_de_caracteres;
+?>
