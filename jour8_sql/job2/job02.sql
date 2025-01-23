2e17b99a00ddc8f74965c5fa0d6927ba5e97c93b
khouloudRais <khouloud.kechichhe-rais@laplateforme.io>
2025-01-23 10:33:48 +0100

    jour8_ job2

---
+++ jour8_sql/job2/job02.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 23 jan. 2025 à 10:31
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `jour8`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salles`
+--
+
+CREATE TABLE `salles` (
+  `id` int(11) DEFAULT NULL,
+  `nom` varchar(255) NOT NULL,
+  `id_etage` int(11) NOT NULL,
+  `capacite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `salles`
+--
+
+INSERT INTO `salles` (`id`, `nom`, `id_etage`, `capacite`) VALUES
+(1, 'lounge', 1, 100),
+(2, 'Studio Son', 1, 5),
+(3, 'Broadcasting', 2, 50),
+(4, 'Bocal Peda', 2, 4),
+(5, 'Coworking', 2, 80),
+(6, 'Studio Video', 2, 5),
+(1, 'lounge', 1, 100),
+(2, 'Studio Son', 1, 5),
+(3, 'Broadcasting', 2, 50),
+(4, 'Bocal Peda', 2, 4),
+(5, 'Coworking', 2, 80),
+(6, 'Studio Video', 2, 5);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
